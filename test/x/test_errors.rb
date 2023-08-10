bf702332b8d40d5e272ba2c9d333294d37ab05a2
Erik Berlin <sferik@gmail.com>
2023-08-10 05:11:59 -0700

    Use require_realtive for test_helper

--- test/x/test_errors.rb
+++ test/x/test_errors.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require_relative "../test_helper"
 
 # Tests for errors
 class ErrorsTest < Minitest::Test
--- test/x/test_version.rb
+++ test/x/test_version.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require_relative "../test_helper"
 
 # Tests for X::VERSION module
 class VersionTest < Minitest::Test
