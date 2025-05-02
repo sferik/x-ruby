9e94f3958f08e32a9d1d2e5487ec2cff20164b3d
Erik Berlin <sferik@gmail.com>
2025-05-01 20:39:07 -0700

    Refactor MediaUploaderTest to fix linter errors

--- test/x/media_uploader_test.rb
+++ test/x/media_uploader_test.rb
@@ -4,38 +4,32 @@ require_relative "../../lib/x/media_uploader"
 module X
   class MediaUploaderTest < Minitest::Test
     cover MediaUploader
+    BOUNDARY = "AaB03x".freeze
+    BASE_URL = "https://api.twitter.com/2/media/upload".freeze
+    MEDIA = {"id" => TEST_MEDIA_ID}.freeze
+    JSON_BODY = {"data" => MEDIA}.to_json.freeze
+    JSON_HEADERS = {"content-type" => "application/json"}.freeze
 
     def setup
       @client = Client.new
-      @media = {"id" => TEST_MEDIA_ID}
-      @data = {"data" => @media}
     end
 
     def test_upload
       file_path = "test/sample_files/sample.jpg"
-      stub_request(:post, "https://api.twitter.com/2/media/upload?media_category=#{MediaUploader::TWEET_IMAGE}")
-        .to_return(body: @media.to_json, headers: {"Content-Type" => "application/json"})
+      stub_request(:post, "#{BASE_URL}?media_category=#{MediaUploader::TWEET_IMAGE}").to_return(body: MEDIA.to_json, headers: JSON_HEADERS)
+      response = MediaUploader.upload(client: @client, file_path:, media_category: MediaUploader::TWEET_IMAGE, boundary: BOUNDARY)
 
-      result = MediaUploader.upload(client: @client, file_path:, media_category: MediaUploader::TWEET_IMAGE, boundary: "AaB03x")
-
-      assert_equal TEST_MEDIA_ID, result["id"]
+      assert_equal TEST_MEDIA_ID, response["id"]
     end
 
     def test_chunked_upload
       file_path = "test/sample_files/sample.mp4"
       total_bytes = File.size(file_path)
       chunk_size_mb = (total_bytes - 1) / MediaUploader::BYTES_PER_MB.to_f
-      stub_request(:post, "https://api.twitter.com/2/media/upload/initialize").with(
-        body: {
-          media_type: "video/mp4",
-          media_category: "tweet_video",
-          total_bytes: total_bytes
-        }.to_json
-      ).to_return(status: 202, headers: {"content-type" => "application/json"}, body: @data.to_json)
-      stub_request(:post, "https://api.twitter.com/2/media/upload/#{TEST_MEDIA_ID}/append").to_return(status: 204)
-      stub_request(:post, "https://api.twitter.com/2/media/upload/#{TEST_MEDIA_ID}/finalize")
-        .to_return(status: 201, headers: {"content-type" => "application/json"}, body: @data.to_json)
-
+      body_json = {media_type: "video/mp4", media_category: "tweet_video", total_bytes:}.to_json
+      stub_request(:post, "#{BASE_URL}/initialize").with(body: body_json).to_return(status: 202, headers: JSON_HEADERS, body: JSON_BODY)
+      stub_request(:post, "#{BASE_URL}/#{TEST_MEDIA_ID}/append").to_return(status: 204)
+      stub_request(:post, "#{BASE_URL}/#{TEST_MEDIA_ID}/finalize").to_return(status: 201, headers: JSON_HEADERS, body: JSON_BODY)
       response = MediaUploader.chunked_upload(client: @client, file_path:, media_category: MediaUploader::TWEET_VIDEO, chunk_size_mb:)
 
       assert_equal TEST_MEDIA_ID, response["id"]
@@ -44,71 +38,53 @@ module X
     def test_append_method
       file_path = "test/sample_files/sample.mp4"
       file_paths = MediaUploader.send(:split, file_path, File.size(file_path) - 1)
-
-      stub_request(:post, "https://api.twitter.com/2/media/upload/#{TEST_MEDIA_ID}/append")
-        .with(headers: {"Content-Type" => "multipart/form-data, boundary=AaB03x"}).to_return(status: 204)
-      MediaUploader.send(:append, client: @client, file_paths:, media: @media, media_type: "video/mp4", boundary: "AaB03x")
+      headers = {"Content-Type" => "multipart/form-data, boundary=#{BOUNDARY}"}
+      stub_request(:post, "#{BASE_URL}/#{TEST_MEDIA_ID}/append").with(headers:).to_return(status: 204)
+      MediaUploader.send(:append, client: @client, file_paths:, media: MEDIA, media_type: "video/mp4", boundary: BOUNDARY)
 
       bodies = []
-      assert_requested(:post, "https://api.twitter.com/2/media/upload/#{TEST_MEDIA_ID}/append", times: 2) do |request|
-        bodies << request.body
-      end
+      assert_requested(:post, "https://api.twitter.com/2/media/upload/#{TEST_MEDIA_ID}/append", times: 2) { |request| bodies << request.body }
 
-      file_paths.each_with_index do |_, segment_index|
+      file_paths.each_index do |segment_index|
         assert(bodies.one? { |body| body.include?("Content-Disposition: form-data; name=\"segment_index\"\r\n\r\n#{segment_index}\r\n") })
       end
     end
 
     def test_await_processing
       stub_request(:get, "https://api.twitter.com/2/media/upload?command=STATUS&media_id=#{TEST_MEDIA_ID}")
-        .to_return(headers: {"content-type" => "application/json"}, body: '{"data":{"processing_info": {"state": "pending"}}}')
-        .to_return(headers: {"content-type" => "application/json"}, body: '{"data":{"processing_info": {"state": "succeeded"}}}')
-      result = MediaUploader.await_processing(client: @client, media: @media)
+        .to_return(headers: JSON_HEADERS, body: '{"data":{"processing_info": {"state": "pending"}}}')
+        .to_return(headers: JSON_HEADERS, body: '{"data":{"processing_info": {"state": "succeeded"}}}')
+      response = MediaUploader.await_processing(client: @client, media: MEDIA)
 
-      assert_equal "succeeded", result["processing_info"]["state"]
+      assert_equal "succeeded", response["processing_info"]["state"]
     end
 
-    def test_await_processing_and_failed
+    def test_await_processing_failed
       stub_request(:get, "https://api.twitter.com/2/media/upload?command=STATUS&media_id=#{TEST_MEDIA_ID}")
-        .to_return(headers: {"content-type" => "application/json"}, body: '{"data":{"processing_info": {"state": "pending"}}}')
-        .to_return(headers: {"content-type" => "application/json"}, body: '{"data":{"processing_info": {"state": "failed"}}}')
-      result = MediaUploader.await_processing(client: @client, media: @media)
+        .to_return(headers: JSON_HEADERS, body: '{"data":{"processing_info": {"state": "pending"}}}')
+        .to_return(headers: JSON_HEADERS, body: '{"data":{"processing_info": {"state": "failed"}}}')
+      response = MediaUploader.await_processing(client: @client, media: MEDIA)
 
-      assert_equal "failed", result["processing_info"]["state"]
+      assert_equal "failed", response["processing_info"]["state"]
     end
 
     def test_retry
       file_path = "test/sample_files/sample.mp4"
-      stub_request(:post, "https://api.twitter.com/2/media/upload/initialize").with(
-        body: {
-          media_type: "video/mp4",
-          media_category: "tweet_video",
-          total_bytes: File.size(file_path)
-        }.to_json
-      ).to_return(status: 202, headers: {"content-type" => "application/json"}, body: @data.to_json)
-      stub_request(:post, "https://api.twitter.com/2/media/upload/#{TEST_MEDIA_ID}/append")
-        .to_return(status: 500).to_return(status: 204)
-      stub_request(:post, "https://api.twitter.com/2/media/upload/#{TEST_MEDIA_ID}/finalize")
-        .to_return(status: 201, headers: {"content-type" => "application/json"}, body: @data.to_json)
+      body_json = {media_type: "video/mp4", media_category: "tweet_video", total_bytes: File.size(file_path)}.to_json
+      stub_request(:post, "#{BASE_URL}/initialize").with(body: body_json).to_return(status: 202, headers: JSON_HEADERS, body: JSON_BODY)
+      stub_request(:post, "#{BASE_URL}/#{TEST_MEDIA_ID}/append").to_return(status: 500).to_return(status: 204)
+      stub_request(:post, "#{BASE_URL}/#{TEST_MEDIA_ID}/finalize").to_return(status: 201, headers: JSON_HEADERS, body: JSON_BODY)
 
       assert MediaUploader.chunked_upload(client: @client, file_path:, media_category: MediaUploader::TWEET_VIDEO)
     end
 
-    def test_validate_with_valid_params
+    def test_validate
       file_path = "test/sample_files/sample.jpg"
 
       assert_nil MediaUploader.send(:validate!, file_path:, media_category: MediaUploader::TWEET_IMAGE)
-    end
-
-    def test_validate_with_invalid_file_path
-      file_path = "invalid/file/path"
       assert_raises(RuntimeError) do
-        MediaUploader.send(:validate!, file_path:, media_category: MediaUploader::TWEET_IMAGE)
+        MediaUploader.send(:validate!, file_path: "bad/path", media_category: MediaUploader::TWEET_IMAGE)
       end
-    end
-
-    def test_validate_with_invalid_media_category
-      file_path = "test/sample_files/sample.jpg"
       assert_raises(ArgumentError) do
         MediaUploader.send(:validate!, file_path:, media_category: "invalid_category")
       end
