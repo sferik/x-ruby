754b5cbbdc31ecddc99a962729c09ca43122191f
GCorbel <guirec.corbel@gmail.com>
2025-06-03 18:47:55 -0400

    readd space between lines

--- lib/x/media_uploader.rb
+++ lib/x/media_uploader.rb
@@ -21,7 +21,7 @@ module X
       boundary: SecureRandom.hex)
       validate!(file_path:, media_category:)
       upload_body = construct_upload_body(file_path:, media_type:, media_category:, boundary:)
-      headers = {"Content-Type" => "multipart/form-data; boundary=#{boundary}"}
+      headers = {"Content-Type" => "multipart/form-data, boundary=#{boundary}"}
       client.post("media/upload", upload_body, headers:)&.fetch("data")
     end
 
@@ -30,7 +30,7 @@ module X
       validate!(file_path:, media_category:)
       media = init(client:, file_path:, media_type:, media_category:)
       chunk_size = chunk_size_mb * BYTES_PER_MB
-      append(client:, file_paths: split(file_path, chunk_size), media:, boundary:)
+      append(client:, file_paths: split(file_path, chunk_size), media:, media_type:, boundary:)
       client.post("media/upload/#{media["id"]}/finalize")&.fetch("data")
     end
 
@@ -46,6 +46,7 @@ module X
     def await_processing!(client:, media:)
       status = await_processing(client:, media:)
       raise "Media processing failed" if status["processing_info"]["state"] == "failed"
+
       status
     end
 
@@ -53,7 +54,9 @@ module X
 
     def validate!(file_path:, media_category:)
       raise "File not found: #{file_path}" unless File.exist?(file_path)
+
       return if MEDIA_CATEGORIES.include?(media_category.downcase)
+
       raise ArgumentError, "Invalid media_category: #{media_category}. Valid values: #{MEDIA_CATEGORIES.join(", ")}"
     end
 
@@ -82,11 +85,11 @@ module X
       client.post("media/upload/initialize", data)&.fetch("data")
     end
 
-    def append(client:, file_paths:, media:, boundary: SecureRandom.hex)
+    def append(client:, file_paths:, media:, media_type:, boundary: SecureRandom.hex)
       threads = file_paths.map.with_index do |file_path, index|
         Thread.new do
-          upload_body = construct_upload_body(file_path:, segment_index: index, boundary:)
-          headers = {"Content-Type" => "multipart/form-data; boundary=#{boundary}"}
+          upload_body = construct_upload_body(file_path:, media_type:, segment_index: index, boundary:)
+          headers = {"Content-Type" => "multipart/form-data, boundary=#{boundary}"}
           upload_chunk(client:, media_id: media["id"], upload_body:, file_path:, headers:)
         end
       end
@@ -108,15 +111,14 @@ module X
       Dir.delete(dirname) if Dir.empty?(dirname)
     end
 
-    def construct_upload_body(file_path:, media_type: nil, media_category: nil, segment_index: nil,
+    def construct_upload_body(file_path:, media_type:, media_category: nil, segment_index: nil,
       boundary: SecureRandom.hex)
       body = ""
       body += "--#{boundary}\r\nContent-Disposition: form-data; name=\"segment_index\"\r\n\r\n#{segment_index}\r\n" if segment_index
       body += "--#{boundary}\r\nContent-Disposition: form-data; name=\"media_category\"\r\n\r\n#{media_category}\r\n" if media_category
-      body += "--#{boundary}\r\nContent-Disposition: form-data; name=\"media_type\"\r\n\r\n#{media_type}\r\n" if media_type
       "#{body}--#{boundary}\r\n" \
         "Content-Disposition: form-data; name=\"media\"; filename=\"#{File.basename(file_path)}\"\r\n" \
-        "Content-Type: #{DEFAULT_MIME_TYPE}\r\n\r\n" \
+        "Content-Type: #{media_type}\r\n\r\n" \
         "#{File.binread(file_path)}\r\n" \
         "--#{boundary}--\r\n"
     end
